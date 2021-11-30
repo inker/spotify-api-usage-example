0e3023980d328b2d2333cc52b8990a277a5385fb
inker <anton.veselyev@gmail.com>
2021-11-30 19:46:49 +0000

    fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SPOTIFY_AUTH_HOST: 'https://accounts.spotify.com/authorize',
+  SPOTIFY_AUTH_HOST: '"https://accounts.spotify.com/authorize"',
   SPOTIFY_CLIENT_ID: '"e900798eba474a8c8ac4c4373b175d7c"',
   SPOTIFY_REDIRECT_URI: '"http://localhost:9583"',
 })
