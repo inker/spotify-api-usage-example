1ced02497f57c2faa82904a0d5a32fd8b0067a95
inker <anton.veselyev@gmail.com>
2021-12-01 00:26:34 +0000

    set minOffset to 200

--- src/ui/Resize.vue
+++ src/ui/Resize.vue
@@ -68,7 +68,7 @@ export default {
         || document.documentElement.clientWidth
         || document.body.clientWidth
 
-      const minOffset = Math.max(width * 0.1, 225)
+      const minOffset = 200
       const maxOffset = width * 0.4
       const offset = Math.round(clamp(e.clientX, minOffset, maxOffset))
       this.setX(`${offset}px`)
